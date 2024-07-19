32240f7c5db28ac1d89a361ef52e314fa7bc0ad6
oladosuabayomi <oladosuabayomi6@gmail.com>
2024-07-19 23:44:10 +0100

    Updated script for form handling

--- script.js
+++ script.js
@@ -16,6 +16,52 @@ function hideSidebar() {
   sidebar.style.display = 'none';
 }
 
+
+/* form handling */
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contact-form');
+    const thankYouMessage = document.getElementById('thank-you-message');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        const formData = new FormData(form);
+
+        fetch(form.action, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                form.style.display = 'none'; // Hide the form
+                thankYouMessage.style.display = 'block'; // Show the thank you message
+                
+                setTimeout(() => {
+                  thankYouMessage.style.display = 'none'; // Hide the thank you message
+                  form.reset(); // Reset the form fields
+                  form.style.display = 'block'; // Show the form again with animation
+                  form.classList.add('fade-in');
+              }, 3000);
+
+            } else {
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        alert('There was a problem with your submission: ' + data["errors"].map(error => error["message"]).join(", "));
+                    } else {
+                        alert('Oops! There was a problem submitting your form');
+                    }
+                });
+            }
+        })
+        .catch(error => {
+            alert('Oops! There was a problem submitting your form');
+        });
+    });
+});
+
 /* document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.getElementById('menu-icon');
     const navList = document.querySelector('.navlist');
